a9c064c303d02331752733b9ac69219b8bcdf777
bitirimer0 <erolsnl@gmail.com>
2017-07-31 01:18:24 +0300

    Templates Uygulama Çalışmaları

---
+++ NorthwndWeb/Entities/Botting.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Net;
+using System.Security.Policy;
+using System.Web;
+using HtmlAgilityPack;
+namespace NorthwndWeb.Entities
+{
+    public class Botting
+    {
+        
+    }
+}
